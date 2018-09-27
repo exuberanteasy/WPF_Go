085beb8275e1986ad7dea18bfc010d4ca4afec3f
exuberanteasy <exuberanteasy@outlook.com>
2018-09-27 17:43:25 +0800

    Update base.cs

--- base.cs
+++ base.cs
@@ -12,3 +12,26 @@ private void button2_Click(object sender, EventArgs e)
 {
     this.myButton3.FillColor1 = Color.Red;
 }
+
+private void myButton1_Click(object sender, EventArgs e)
+{
+    AdventureWorksEntities dbContext = new AdventureWorksEntities();
+
+    var q = from p in dbContext.ProductPhotoes
+            select p;
+
+    List<ProductPhoto> productList = q.ToList();
+
+    this.dataGridView1.DataSource = productList;
+
+    for (int i = 0; i < productList.Count -1; i++)
+    {
+        MyItemTemplate MIT = new MyItemTemplate();
+
+        MIT.Desc = productList[i].ModifiedDate.ToLongDateString();
+        MIT.ImageBytes = productList[i].LargePhoto;
+        this.flowLayoutPanel1.Controls.Add(MIT);
+
+        Application.DoEvents(); //同時跟者丟的意思
+    }
+}
