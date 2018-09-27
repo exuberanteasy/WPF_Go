c5ac94129ca618e07b39a17d5716fed2dfee6fda
exuberanteasy <exuberanteasy@outlook.com>
2018-09-27 18:49:01 +0800

    Update base.cs

--- base.cs
+++ base.cs
@@ -35,3 +35,25 @@ private void myButton1_Click(object sender, EventArgs e)
         Application.DoEvents(); //同時跟者丟的意思
     }
 }
+
+private void myButton2_Click(object sender, EventArgs e)
+{
+    List<PhotoDataItem> productList = PhotoDataSource.Search(SearchTerm: "Microsoft", results: 100);
+
+    this.dataGridView1.DataSource = productList;
+
+    for (int i = 0; i < productList.Count - 1; i++)
+    {
+        MyItemTemplate MIT = new MyItemTemplate();
+
+        MIT.Desc = productList[i].Title;
+        MIT.ImagePath = productList[i].ImagePath;
+        this.flowLayoutPanel1.Controls.Add(MIT);
+
+        Application.DoEvents();  //加了這行比較屌
+
+    }
+ }
+
+
+
