711c9caa16f996d0701439af410dbb9e7b090b62
exuberanteasy <exuberanteasy@outlook.com>
2018-09-27 17:31:09 +0800

    Update base.cs

--- base.cs
+++ base.cs
@@ -1,7 +1,14 @@
 // 20180927
+// Solution1
 
+ //set property
 private void button1_Click(object sender, EventArgs e)
 {
-    //set property
     this.myButton3.FillColor2 = Color.Yellow;
+}
+
+//set property
+private void button2_Click(object sender, EventArgs e)
+{
+    this.myButton3.FillColor1 = Color.Red;
 }
