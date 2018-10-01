388b3efacc93976be7f61de56bc9bb0327d08170
exuberanteasy <exuberanteasy@outlook.com>
2018-10-01 09:55:29 +0800

    Create base_2.cs

---
+++ base_2.cs
@@ -0,0 +1,31 @@
+// 20181001  酷炫視窗
+
+
+System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            this.ShutdownMode = ShutdownMode.OnLastWindowClose;
+            MainWindow w = new MainWindow();
+            w.Show();
+        }
+    }
+}
